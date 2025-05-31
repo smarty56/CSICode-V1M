0fbf528b7b62f832caef73a74fbebcbbf1e9485f
FunkybotsEvilTwin <52307138+FunkybotsEvilTwin@users.noreply.github.com>
2025-05-30 21:36:15 -0400

    Const Char Fix

--- reaper_csurf_integrator/control_surface_midi_widgets.h
+++ reaper_csurf_integrator/control_surface_midi_widgets.h
@@ -1495,7 +1495,7 @@ public:
         //---------------------------------------------------
         // HANDLE METER SPECIFIC SCALING, DEFAULT IS "XTOUCH"
         //---------------------------------------------------
-        char* meterMode = nullptr;
+        const char* meterMode = nullptr;
 
         PropertyType propertyType = properties.prop_from_string("MeterMode");
         if (propertyType)
